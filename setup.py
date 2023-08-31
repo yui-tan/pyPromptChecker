18d95553ed1a02ed6e63da33508d0c96dc9c4592
Yui-tan <121333129+yui-tan@users.noreply.github.com>
2023-08-31 21:07:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     install_requires=[
         'pyQt6',
         'pypng',
-        'pillow'
+        'pillow',
+        'pyqtdarktheme'
     ],
     entry_points={
         'console_scripts': [
